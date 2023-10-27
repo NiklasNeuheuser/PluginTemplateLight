1becde9fb350b7baa03e2788ed37e21af4988e87
NiklasNeuheuser <michel03@t-online.de>
2023-10-27 17:35:33 +0200

    Initial commit

---
+++ src/main/java/org/mineacademy/template/PluginTemplate.java
@@ -0,0 +1,49 @@
+package org.mineacademy.template;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+/**
+ * PluginTemplate is a simple template you can use every time you make
+ * a new plugin. This will save you time because you no longer have to
+ * recreate the same skeleton and features each time.
+ * <p>
+ * It uses Foundation for fast and efficient development process.
+ */
+public final class PluginTemplate extends SimplePlugin {
+
+	/**
+	 * Automaticaly performs login  Once when the plugin starts
+	 */
+	int aplle = 5;
+
+	@Override
+	protected void onPluginStart() {
+
+
+		System.out.println(2332);
+		System.out.println("Hallo");
+		System.out.println("Kuhl");
+
+
+	}
+
+
+	/* ------------------------------------------------------------------------------- */
+	/* Events */
+	/* ------------------------------------------------------------------------------- */
+
+	/**
+	 * An example event that checks if the right clicked entity is a cow, and makes an explosion.
+	 * You can write your events to your main class without having to register a listener.
+	 *
+	 * @param event
+	 */
+	@EventHandler
+	public void onRightClick(PlayerInteractEntityEvent event) {
+		if (event.getRightClicked().getType() == EntityType.PIG)
+			event.getRightClicked().getWorld().createExplosion(event.getRightClicked().getLocation(), 16);
+	}
+}
